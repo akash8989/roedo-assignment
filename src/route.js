9e612c344560b128da334da4ca489b51fb09a877
akash singh <akashcool271995>
2022-07-29 15:46:24 +0530

    assignment

---
+++ src/route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+// const oxford-dictionary = 
+
+const apiController= require('../controllers/apiController')
+
+router.get('/')
+ 
+module.exports = router;
